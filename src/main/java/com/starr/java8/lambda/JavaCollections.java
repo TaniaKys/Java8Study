513e27284fcf0599deec7710668a79c5a7fe8283
tetianad <always_ultra@ukr.net>
2017-04-24 16:34:55 +0300

    print array, using forEach and Consumer interface

--- src/main/java/com/starr/java8/lambda/JavaCollections.java
+++ src/main/java/com/starr/java8/lambda/JavaCollections.java
@@ -19,6 +19,12 @@ public class JavaCollections {
         //simple sort
         Collections.sort(list);
 
+        System.out.println("Simple sort:");
+        //simple array print
+        for (String str : list) {
+            System.out.println(str);
+        }
+
         //sort with lambda comporator v.1
         Comparator<String> comp = (str1, str2) -> {
           return str1.compareToIgnoreCase(str2);
@@ -34,10 +40,12 @@ public class JavaCollections {
         //sort with lambda comporator v.3 (for future)
         //Collections.sort(list, String::compareToIgnoreCase);
 
-        for (String str : list) {
-            System.out.println(str);
-        }
 
-        
+        System.out.println("Corporator sort:");
+        //array print with lambda (using Consumer interface)
+        list.forEach(str -> System.out.println(str));
+
+
+
     }
 }
